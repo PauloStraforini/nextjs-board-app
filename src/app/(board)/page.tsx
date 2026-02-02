2f351b93faa08f4c78f68794fabe40e7ffe4c135
PauloStraforini <paulostraforini@gmail.com>
2026-02-02 15:36:40 -0300

    Buscando Issues do Board

--- src/app/(board)/page.tsx
+++ src/app/(board)/page.tsx
@@ -3,6 +3,7 @@ import { Section } from "@/components/section"
 import { Card } from "@/components/card"
 import { Button } from "@/components/button"
 import { Metadata } from "next"
+import { listIssues } from "@/http/list-issues"
 
 export const metadata: Metadata = {
   title: "Board",
@@ -17,11 +18,15 @@ interface BoardProps {
 export default async function Board({ searchParams }: BoardProps) {
   const { q } = await searchParams
 
+  const issues = await listIssues()
+
   return (
     <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
       <div></div>
 
       <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+
+        {/* Backlog */}
         <Section.Root>
           {/* Header */}
           <Section.Header>
@@ -30,29 +35,150 @@ export default async function Board({ searchParams }: BoardProps) {
               Backlog
             </Section.Title>
 
-            <Section.IssueCount>10</Section.IssueCount>
+            <Section.IssueCount>{issues.backlog.length}</Section.IssueCount>
           </Section.Header>
 
           {/* Content */}
           <Section.Content>
-            <Card.Root>
-              <Card.Header>
-                <Card.Number>ECO-123</Card.Number>
-                <Card.Title>Implementar cartao de credito</Card.Title>
-                <Card.Footer>
-                  <Button>
-                    <ThumbsUpIcon className="size-3" />
-                    <span className="text-sm">13</span>
-                  </Button>
+            {issues.backlog.map(issue => {
+              return (
+                <Card.Root key={issue.id} >
+                  <Card.Header>
+                    <Card.Number>ISS-{issue.id}</Card.Number>
+                    <Card.Title>{issue.title}</Card.Title>
+                    <Card.Footer>
+                      <Button>
+                        <ThumbsUpIcon className="size-3" />
+                        <span className="text-sm">13</span>
+                      </Button>
 
-                  <Button>
-                    <MessageCircleIcon className="size-3" />
-                    <span className="text-sm">3</span>
-                  </Button>
+                      <Button>
+                        <MessageCircleIcon className="size-3" />
+                        <span className="text-sm">{issue.comments}</span>
+                      </Button>
 
-                </Card.Footer>
-              </Card.Header>
-            </Card.Root>
+                    </Card.Footer>
+                  </Card.Header>
+                </Card.Root>
+              )
+            })}
+          </Section.Content>
+        </Section.Root>
+
+        {/* To-do */}
+        <Section.Root>
+          {/* Header */}
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              To-do
+            </Section.Title>
+
+            <Section.IssueCount>{issues.todo.length}</Section.IssueCount>
+          </Section.Header>
+
+          {/* Content */}
+          <Section.Content>
+            {issues.todo.map(issue => {
+              return (
+                <Card.Root key={issue.id} >
+                  <Card.Header>
+                    <Card.Number>ISS-{issue.id}</Card.Number>
+                    <Card.Title>{issue.title}</Card.Title>
+                    <Card.Footer>
+                      <Button>
+                        <ThumbsUpIcon className="size-3" />
+                        <span className="text-sm">13</span>
+                      </Button>
+
+                      <Button>
+                        <MessageCircleIcon className="size-3" />
+                        <span className="text-sm">{issue.comments}</span>
+                      </Button>
+
+                    </Card.Footer>
+                  </Card.Header>
+                </Card.Root>
+              )
+            })}
+          </Section.Content>
+        </Section.Root>
+
+        {/* In Pogress */}
+        <Section.Root>
+          {/* Header */}
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              In pogress
+            </Section.Title>
+
+            <Section.IssueCount>{issues.in_progress.length}</Section.IssueCount>
+          </Section.Header>
+
+          {/* Content */}
+          <Section.Content>
+            {issues.in_progress.map(issue => {
+              return (
+                <Card.Root key={issue.id} >
+                  <Card.Header>
+                    <Card.Number>ISS-{issue.id}</Card.Number>
+                    <Card.Title>{issue.title}</Card.Title>
+                    <Card.Footer>
+                      <Button>
+                        <ThumbsUpIcon className="size-3" />
+                        <span className="text-sm">13</span>
+                      </Button>
+
+                      <Button>
+                        <MessageCircleIcon className="size-3" />
+                        <span className="text-sm">{issue.comments}</span>
+                      </Button>
+
+                    </Card.Footer>
+                  </Card.Header>
+                </Card.Root>
+              )
+            })}
+          </Section.Content>
+        </Section.Root>
+
+        {/* Done */}
+        <Section.Root>
+          {/* Header */}
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              Done
+            </Section.Title>
+
+            <Section.IssueCount>{issues.done.length}</Section.IssueCount>
+          </Section.Header>
+
+          {/* Content */}
+          <Section.Content>
+            {issues.done.map(issue => {
+              return (
+                <Card.Root key={issue.id} >
+                  <Card.Header>
+                    <Card.Number>ISS-{issue.id}</Card.Number>
+                    <Card.Title>{issue.title}</Card.Title>
+                    <Card.Footer>
+                      <Button>
+                        <ThumbsUpIcon className="size-3" />
+                        <span className="text-sm">13</span>
+                      </Button>
+
+                      <Button>
+                        <MessageCircleIcon className="size-3" />
+                        <span className="text-sm">{issue.comments}</span>
+                      </Button>
+
+                    </Card.Footer>
+                  </Card.Header>
+                </Card.Root>
+              )
+            })}
           </Section.Content>
         </Section.Root>
       </main>
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,13 +1,13 @@
 import type { ComponentProps } from "react"
 import { twMerge } from "tailwind-merge"
 
-interface SectionRootProps extends ComponentProps<"div"> {}
+interface SectionRootProps extends ComponentProps<"div"> { }
 
 function SectionRoot({ className, ...props }: SectionRootProps) {
   return (
     <div
       className={twMerge(
-        "bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1",
+        "bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1 relative",
         className,
       )}
       {...props}
@@ -15,7 +15,7 @@ function SectionRoot({ className, ...props }: SectionRootProps) {
   )
 }
 
-interface SectionHeaderProps extends ComponentProps<"div"> {}
+interface SectionHeaderProps extends ComponentProps<"div"> { }
 
 function SectionHeader({ className, ...props }: SectionHeaderProps) {
   return (
@@ -26,7 +26,7 @@ function SectionHeader({ className, ...props }: SectionHeaderProps) {
   )
 }
 
-interface SectionTitleProps extends ComponentProps<"span"> {}
+interface SectionTitleProps extends ComponentProps<"span"> { }
 
 function SectionTitle({ className, ...props }: SectionTitleProps) {
   return (
@@ -40,7 +40,7 @@ function SectionTitle({ className, ...props }: SectionTitleProps) {
   )
 }
 
-interface SectionIssueCountProps extends ComponentProps<"span"> {}
+interface SectionIssueCountProps extends ComponentProps<"span"> { }
 
 function SectionIssueCount({ className, ...props }: SectionIssueCountProps) {
   return (
@@ -48,13 +48,13 @@ function SectionIssueCount({ className, ...props }: SectionIssueCountProps) {
   )
 }
 
-interface SectionContentProps extends ComponentProps<"div"> {}
+interface SectionContentProps extends ComponentProps<"div"> { }
 
 function SectionContent({ className, ...props }: SectionContentProps) {
   return (
     <div
       className={twMerge(
-        "flex flex-col gap-2.5 overflow-y-scroll p-3",
+        "flex flex-col gap-2.5 overflow-y-auto p-3 absolute inset-0 top-12 scrollbar scrollbar-thumb-navy-600 scrollbar-track-transparent",
         className,
       )}
       {...props}
